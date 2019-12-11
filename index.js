07945a913fcd01e21421cb2d464f0c6e49a0d063
Pasha <admin@MacBook-Pro-admin.local>
2019-12-12 01:09:35 +0200

    add the template of site

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars")
+const todoRoutes = require("./routes/todos")
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout:'main',
+    extname:'hbs' 
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(todoRoutes)
+
+
 function start(){
-    mongoose.connect('', {
+    mongoose.connect('mongodb+srv://Pasha:12pasha04@cluster0-ylqzb.mongodb.net/todos', {
         useNewUrlParser:true,
-        useFindAndModify:false
+        useFindAndModify:false,
+        useUnifiedTopology: true
+        
     })
     app.listen(PORT, () => {
         console.log("Server has been started...")
