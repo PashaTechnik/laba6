18e825fc505aee177ae5ff0d240946449e8a13fc
Pasha <admin@MacBook-Pro-admin.local>
2019-12-13 10:48:56 +0200

    fix 2.0

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const todoRoutes = require("./routes/todos");
 const path = require("path");
-const MongoClient = require('mongodb').MongoClient;
 
 const PORT = process.env.PORT || 3000
 
